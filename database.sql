f423396d598bf651a22b8793b0ba229ebc96fa8a
Leen <Leena.kraytem@gmail.com>
2026-03-06 10:03:15 -0500

    added data types to tables

--- database.sql
+++ database.sql
@@ -1,26 +1,26 @@
 CREATE TABLE celebrities (
-    celebrityId
-    name
-    profession
-    nationality
+    celebrityId PRIMARY KEY,
+    name VARCHAR(100),
+    profession VARCHAR(100),
+    nationality VARCHAR(100),
 )        
 
 CREATE TABLE planeModels (
-    planeId
-    manufacturer
-    modelName
-    manufacturingYear
-    capacity
-    maxRangeKm
+    planeId PRIMARY KEY,
+    manufacturer VARCHAR(100),
+    modelName VARCHAR(100),
+    manufacturingYear INTEGER,
+    capacity INTEGER,
+    maxRangeKm INTEGER,
 )
 
 CREATE TABLE flights (
-    flightID
-    celebrityId
-    planeId
-    flightNumber
-    departureAirport
-    arrivalAirport
-    departureTime
-    arrivalTime
+    flightID PRIMARY KEY,
+    celebrityId REFERENCES celebrities(celebrityId),
+    planeId REFERENCES planeModels(planeId),
+    flightNumber VARCHAR(20),
+    departureAirport VARCHAR(50),
+    arrivalAirport VARCHAR(50),
+    departureTime TIMESTAMP,
+    arrivalTime TIMESTAMP
 )
