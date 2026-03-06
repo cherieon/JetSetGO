b2c6e342761e7dc0bc499e1b3cce68c53ae5ebaa
Leen <Leena.kraytem@gmail.com>
2026-03-06 09:56:48 -0500

    created tables

---
+++ database.sql
@@ -0,0 +1,26 @@
+CREATE TABLE celebrities (
+    celebrityId
+    name
+    profession
+    nationality
+)        
+
+CREATE TABLE planeModels (
+    planeId
+    manufacturer
+    modelName
+    manufacturingYear
+    capacity
+    maxRangeKm
+)
+
+CREATE TABLE flights (
+    flightID
+    celebrityId
+    planeId
+    flightNumber
+    departureAirport
+    arrivalAirport
+    departureTime
+    arrivalTime
+)
